539e2a2a46610d7a2eb921f0d29c3f63b5ebed34
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-31 17:02:58 +0900

    [Feature] 무한 맵 기본 설정
    맵 오브젝트는 4개를 생성하고 Z좌표를 0, 30, 60, 90으로 설정하려고 생각중입니다.

--- Assets/Scripts/Map/MapScroller.cs
+++ Assets/Scripts/Map/MapScroller.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MapScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+
+    private float minSpeed;
+    private float maxSpeed;
+
+    private Vector3 targetPosition;
+
+    private void Start()
     {
-        
+        minSpeed = speed;
+        maxSpeed = 40f;
+        targetPosition = new Vector3(0, 0, -29.9f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        BreakSpeed();
+
+        if (transform.position.z <= targetPosition.z)
+        {
+            transform.position = new Vector3(0, 0, 90);
+        }
+
+        transform.position = Vector3.MoveTowards
+            (transform.position, targetPosition, speed * Time.fixedDeltaTime);
+    }
+
+    public void BreakSpeed()
+    {
+        // ToDo :: 매개변수
+        bool isPlayerDamage = false;
+
+        if (isPlayerDamage)
+        {
+            speed = minSpeed;
+        }
+       
     }
 }

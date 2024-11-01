5bc99863459eba42fdc3350c75e06b35dabe9aaa
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-11-01 20:03:10 +0900

    [Update] 자석 아이템

--- Assets/Scripts/Item/MagnetItem.cs
+++ Assets/Scripts/Item/MagnetItem.cs
@@ -1,11 +1,34 @@
+using System.Collections;
 using UnityEngine;
 
 public class MagnetItem : ItemBase
 {
-    public override void Use()
+    public override void Use(Player player)
     {
         // 자석 코드 작성
 
         // 플레이어 반경으로 감지해서 코인이 있으면 그 코인이 플레이어 쪽으로 다가가게 함.
+
+
+
+    }
+
+    private IEnumerator MagnetCoroutine()
+    {
+        float magnetDuration = 0;
+
+        while (magnetDuration < duration)
+        {
+            magnetDuration += Time.deltaTime;
+
+            var coins = Physics.OverlapSphere(transform.position, itemValue, LayerMask.NameToLayer("Coin"));
+
+            foreach (var coin in coins)
+            {
+                coin.GetComponent<CoinItem>().EnableMagnet();
+            }
+
+            yield return null;
+        }
     }
 }

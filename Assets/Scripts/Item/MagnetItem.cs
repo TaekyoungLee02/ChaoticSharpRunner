bec45c6b1715803f6b5c6ade3c1913c65ffaeb93
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-11-01 16:23:33 +0900

    [Feature] 자석 아이템

---
+++ Assets/Scripts/Item/MagnetItem.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MagnetItem : ItemBase
+{
+    public override void Use()
+    {
+        // 자석 코드 작성
+
+        // 플레이어 반경으로 감지해서 코인이 있으면 그 코인이 플레이어 쪽으로 다가가게 함.
+    }
+}

783c4cdb3c490a5506b8af7e066def28c9703629
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-11-01 16:21:45 +0900

    [Feature] 아이템 부모 클래스 작성

---
+++ Assets/Scripts/Item/Base/ItemBase.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemBase : MonoBehaviour
+{
+    private string itemName;
+    private string itemDescription;
+
+    protected float itemValue;
+    protected float duration;
+
+    public virtual void Init(ItemSO so)
+    {
+        itemName = so.itemName;
+        itemDescription = so.itemDescription;
+        itemValue = so.itemValue;
+        duration = so.duration;
+    }
+
+    public abstract void Use(); //Player 매개변수로 받음
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Use();
+            // 풀로 돌려놓기
+        }
+    }
+}

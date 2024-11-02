ed7ef6c341b2a6d16f84b4078e831d40955f316c
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-11-03 05:06:18 +0900

    [Update] 코드 정리, PlayerAbility 임시 연결
    Test_Map.cs 제거
    objectPool 오브젝트를 따로 빼놓음

--- Assets/Scripts/Map/MapController.cs
+++ Assets/Scripts/Map/MapController.cs
@@ -7,21 +7,30 @@ public class MapController : MonoBehaviour
     public float speed;
 
     public float ranTime;
-    public float accelerationTime;
+    public float accelerationCoolTime;
 
     public float minSpeed;
     public float maxSpeed;
+    public float saveSpeed;
 
-    public bool playerDamage = false;
+    // TODO :: PlayerAbility
+    bool inPlayerDamage = false;
+    bool inPlayeritem = false;
 
     private void Start()
     {
         minSpeed = speed;
+
+        GameManager.Instance.player.ability.OnSlowDown += ResetSpeed;
+        GameManager.Instance.player.ability.OnRestoreSpeed += AccelerationSpeed;
     }
 
     private void FixedUpdate()
     {
-        ranTime += Time.fixedDeltaTime;
+        if (!inPlayeritem)
+        {
+            ranTime += Time.fixedDeltaTime;
+        }
     }
 
     public float MapSpeed()
@@ -34,28 +43,43 @@ public class MapController : MonoBehaviour
 
     public void ResetSpeed()
     {
-        if (playerDamage)
-        {
+        if (inPlayeritem)
+        { // 아이템 효과가 활성화 상태라면
+            saveSpeed = speed;
+            speed = minSpeed; // TODO :: 감소량 변경
+            // 속도 저장 후 변경
+        }
+        else if(inPlayerDamage && !inPlayeritem)
+        { // 아이템 효과가 없을 때 데미지를 입는다면
             ranTime = 0f;
             speed = minSpeed;
+            // 속도 초기화
         }
 
         if (speed <= minSpeed)
-        {
+        { // 속도 최소치
             speed = minSpeed;
         }
     }
 
     public void AccelerationSpeed()
     {
-        if (ranTime >= accelerationTime)
-        {
+        if (!inPlayeritem && saveSpeed != 0)
+        { // 아이템 효과가 없는데 저장된 속도가 존재한다면
+            speed = saveSpeed;
+            saveSpeed = 0;
+            // 속도를 저장된 값으로 설정 후 초기화
+        }
+
+        if (ranTime >= accelerationCoolTime)
+        { // 가속 쿨타임을 넘었다면
             speed += 1;
             ranTime = 0f;
+            // 가속 후 초기화
         }
 
         if (speed >= maxSpeed)
-        {
+        { // 속도 최대치
             speed = maxSpeed;
         }
     }
--- Assets/Scripts/Map/SpawnController.cs
+++ Assets/Scripts/Map/SpawnController.cs
@@ -22,20 +22,31 @@ public class SpawnController : MonoBehaviour
     public string[] mapNameArray = { "CoinMap", "BaseMap", "errerMap" };
     public List<GameObject> mapObjectArray = new List<GameObject>();
 
+    [SerializeField]
+    public int mapSpawnCount;
+    [SerializeField]
+    public float mapSpawnDistance;
+
     private void Awake()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < mapSpawnCount; i++)
         { // 처음 초기화 시 리스트에 넣으면서 생성
-            GameObject newMapObject = GetMapObject(ObjectPool.Instance);
-            newMapObject.transform.position = new Vector3(0, 0, i * 30);
-            mapObjectArray.Add(newMapObject);
+            StartMapSpawn(i);
         }
     }
 
-    public GameObject GetMapObject(ObjectPool inPool)
+    private void StartMapSpawn(int inSpawnCount)
+    {
+        GameObject newMapObject = GetMapObject();
+        newMapObject.transform.position =
+            new Vector3(0, 0, inSpawnCount * mapSpawnDistance);
+        mapObjectArray.Add(newMapObject);
+    }
+
+    public GameObject GetMapObject()
     { // 오브젝트 랜덤 생성
         int randomMap = Random.Range(0, mapNameArray.Length);
-        GameObject outNewMap = inPool.SpawnFromPool(mapNameArray[randomMap]);
+        GameObject outNewMap = ObjectPool.Instance.SpawnFromPool(mapNameArray[randomMap]);
 
         return outNewMap;
     }

aed792586bd2601cf9e14ccb9e16bb66e13e8525
kkamsogod <9310rlawlgns@gmail.com>
2024-11-06 20:56:52 +0900

    [Chore] 기타 수정작업

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -15,9 +15,9 @@ public class GameManager : Singleton<GameManager>
     public event Action<bool> OnTogglePause;
     public event Action OnGoToTitleScene;
     public event Action OnResumeGame;
+    public Action OnSpeedStart;
 
-    private bool isPaused = false;
-    public bool IsPaused => isPaused;
+    public bool isPaused = false;
     private bool isGameOver = false;
 
     private void Start()
@@ -62,10 +62,9 @@ public class GameManager : Singleton<GameManager>
     public void StartGame()
     {
         OnGameStart?.Invoke();
-        isPaused = false;
+        isPaused = true;
         Time.timeScale = 1;
         isGameOver = false;
-        OnTogglePause?.Invoke(isPaused);
     }
 
     public void GameOver()
